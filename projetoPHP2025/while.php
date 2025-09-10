b8761eaabaf224c7eaf57cd36c2ede0df0292814
Pedro Matos Anselmo <pedroanselmo2005@gmail.com>
2025-09-09 22:04:10 -0300

    atv1

---
+++ projetoPHP2025/while.php
@@ -0,0 +1,15 @@
+<?php 
+	$n1 = 1;
+	$n2 = 5;
+	$n3 = $n1 + $n2;
+
+	if ($n3>20) {
+		echo("$n3 é maior que 20, e o novo numero é ".$n3+8);
+	}
+	elseif ($n3<20) {
+		echo("$n3 é menor que 20, e o novo numero é ".$n3 - 5);
+	}
+
+
+
+ ?>
