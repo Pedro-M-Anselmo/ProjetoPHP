1d641120228b0b93228917f2d2d74bc252a01868
Pedro Matos Anselmo <pedroanselmo2005@gmail.com>
2025-09-09 22:18:23 -0300

    Atv

--- projetoPHP2025/while.php
+++ projetoPHP2025/while.php
@@ -1,4 +1,6 @@
 <?php 
+
+	echo "Exercício 1 <br />"; 
 	$n1 = 1;
 	$n2 = 5;
 	$n3 = $n1 + $n2;
@@ -7,9 +9,20 @@
 		echo("$n3 é maior que 20, e o novo numero é ".$n3+8);
 	}
 	elseif ($n3<20) {
-		echo("$n3 é menor que 20, e o novo numero é ".$n3 - 5);
+		echo("$n3 é menor que 20, e o novo numero é ".$n3 - 5 ."<br /> <br /> <br />");
 	}
 
+
+	echo "exercício 2 <br />";
+
+	$n1 = 5;
+
+	if ($n1 % 2 == 0 ) {
+		echo "é divisível por 2..;";
+	}
+	elseif ($n1 % 5 == 0) {
+		echo "é divisivel pr 5...";
+	}
 
 
  ?>
